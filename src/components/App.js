0b9fe1a03adfce34e246d486756c1cb397d68a96
Blaise-Shyaka <blaiseshyaka95@gmail.com>
2021-06-11 17:10:19 +0200

    Add the Nav Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import BooksList from '../containers/BooksList';
 import BooksForm from '../containers/BooksForm';
+import Nav from './Nav';
 
 function App() {
   return (
     <div>
+      <Nav />
       <BooksForm />
       <BooksList />
     </div>
---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+function Nav() {
+  return (
+    <nav>
+      <h1>BookStore</h1>
+      <ul>
+        <li>BOOKS</li>
+        <li>CATEGORIES</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
